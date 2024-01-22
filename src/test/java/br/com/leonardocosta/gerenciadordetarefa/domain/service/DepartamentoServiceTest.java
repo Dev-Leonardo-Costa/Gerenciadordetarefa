775f84b8814884670401c6ffe8ce761a189a95ac
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2024-01-22 17:09:16 -0300

    test: removendo o construtor do before

--- src/test/java/br/com/leonardocosta/gerenciadordetarefa/domain/service/DepartamentoServiceTest.java
+++ src/test/java/br/com/leonardocosta/gerenciadordetarefa/domain/service/DepartamentoServiceTest.java
@@ -31,67 +31,62 @@ class DepartamentoServiceTest {
     @Mock
     private DepartamentoRepository departamentoRepository;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Test
+    void deveBuscarPorId() {
+        Departamento departamentoMock = new Departamento();
+        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamentoMock));
+
+        Departamento result = departamentoService.buscarPorId(ID_DEPARTAMENTO);
+
+        assertNotNull(result);
+        assertEquals(departamentoMock, result);
+        verify(departamentoRepository, times(1)).findById(ID_DEPARTAMENTO);
     }
 
-//    @Test
-//    void deveBuscarPorId() {
-//        Departamento departamentoMock = new Departamento();
-//        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamentoMock));
-//
-//        Departamento result = departamentoService.buscarPorId(ID_DEPARTAMENTO);
-//
-//        assertNotNull(result);
-//        assertEquals(departamentoMock, result);
-//        verify(departamentoRepository, times(1)).findById(ID_DEPARTAMENTO);
-//    }
 
+    @Test
+    void deveBuscarPorIdDepartamentoParaPessoa() {
+        Departamento departamentoMock = new Departamento();
+        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamentoMock));
 
-//    @Test
-//    void deveBuscarPorIdDepartamentoParaPessoa() {
-//        Departamento departamentoMock = new Departamento();
-//        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamentoMock));
-//
-//        Departamento result = departamentoService.buscarPorIdDepartamentoParaPessoa(ID_DEPARTAMENTO);
-//
-//        assertNotNull(result);
-//        assertEquals(departamentoMock, result);
-//        verify(departamentoRepository, times(1)).findById(ID_DEPARTAMENTO);
-//
-//    }
+        Departamento result = departamentoService.buscarPorIdDepartamentoParaPessoa(ID_DEPARTAMENTO);
 
-//    @Test
-//    void deveListarDepartamento() {
-//        List<DepartamentoProjection> departamentoProjections = Collections.singletonList(mock(DepartamentoProjection.class));
-//        when(departamentoRepository.listarDepartamentosComQuantidadeDePessoasETarefas()).thenReturn(departamentoProjections);
-//
-//        List<DepartamentoDTO> result = departamentoService.listarDepartamento();
-//
-//        verify(departamentoRepository, times(1)).listarDepartamentosComQuantidadeDePessoasETarefas();
-//        assertNotNull(result);
-//    }
+        assertNotNull(result);
+        assertEquals(departamentoMock, result);
+        verify(departamentoRepository, times(1)).findById(ID_DEPARTAMENTO);
 
-//    @Test
-//    void buscarPorIdDeveLancarNotFoundExceptionSeDepartamentoNaoExistir() {
-//        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.empty());
-//
-//        assertThrows(NotFoundException.class, () -> departamentoService.buscarPorId(ID_DEPARTAMENTO));
-//
-//        verify(departamentoRepository, times(1)).findById(ID_DEPARTAMENTO);
-//    }
+    }
 
-//    @Test
-//    void deveBuscarDepartamentoPorIdComSucesso() {
-//        Departamento departamento = new Departamento();
-//        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamento));
-//
-//        Departamento result = departamentoService.buscarPorId(ID_DEPARTAMENTO);
-//
-//        assertNotNull(result);
-//        assertEquals(departamento, result);
-//    }
+    @Test
+    void deveListarDepartamento() {
+        List<DepartamentoProjection> departamentoProjections = Collections.singletonList(mock(DepartamentoProjection.class));
+        when(departamentoRepository.listarDepartamentosComQuantidadeDePessoasETarefas()).thenReturn(departamentoProjections);
+
+        List<DepartamentoDTO> result = departamentoService.listarDepartamento();
+
+        verify(departamentoRepository, times(1)).listarDepartamentosComQuantidadeDePessoasETarefas();
+        assertNotNull(result);
+    }
+
+    @Test
+    void buscarPorIdDeveLancarNotFoundExceptionSeDepartamentoNaoExistir() {
+        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> departamentoService.buscarPorId(ID_DEPARTAMENTO));
+
+        verify(departamentoRepository, times(1)).findById(ID_DEPARTAMENTO);
+    }
+
+    @Test
+    void deveBuscarDepartamentoPorIdComSucesso() {
+        Departamento departamento = new Departamento();
+        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamento));
+
+        Departamento result = departamentoService.buscarPorId(ID_DEPARTAMENTO);
+
+        assertNotNull(result);
+        assertEquals(departamento, result);
+    }
 
     @Test
     void deveLancarNotFoundExceptionAoBuscarDepartamentoPorIdInexistente() {
@@ -100,17 +95,17 @@ class DepartamentoServiceTest {
         assertThrows(NotFoundException.class, () -> departamentoService.buscarPorId(ID_DEPARTAMENTO));
     }
 
-//    @Test
-//    void deveBuscarDepartamentoPorIdDepartamentoParaPessoaComSucesso() {
-//
-//        Departamento departamento = new Departamento();
-//        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamento));
-//
-//        Departamento result = departamentoService.buscarPorIdDepartamentoParaPessoa(ID_DEPARTAMENTO);
-//
-//        assertNotNull(result);
-//        assertEquals(departamento, result);
-//    }
+    @Test
+    void deveBuscarDepartamentoPorIdDepartamentoParaPessoaComSucesso() {
+
+        Departamento departamento = new Departamento();
+        when(departamentoRepository.findById(ID_DEPARTAMENTO)).thenReturn(Optional.of(departamento));
+
+        Departamento result = departamentoService.buscarPorIdDepartamentoParaPessoa(ID_DEPARTAMENTO);
+
+        assertNotNull(result);
+        assertEquals(departamento, result);
+    }
 
     @Test
     void deveLancarNotFoundExceptionAoBuscarDepartamentoPorIdDepartamentoParaPessoaInexistente() {
