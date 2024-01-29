cc516186a1b6bd78025966f32d0661d3ab3591e1
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2024-01-29 20:26:44 -0300

    docs: import sql para add 5 registros na entidade departamento

---
+++ src/test/java/br/com/leonardocosta/gerenciadordetarefa/resources/import.sql
@@ -0,0 +1,6 @@
+-- CREATE DEPARTAMENTO
+INSERT INTO departamento (id, nome) VALUES (1, 'Operações');
+INSERT INTO departamento (id, nome) VALUES (2, 'Recursos Humanos');
+INSERT INTO departamento (id, nome) VALUES (3, 'Finanças');
+INSERT INTO departamento (id, nome) VALUES (4, 'Marketing');
+INSERT INTO departamento (id, nome) VALUES (5, 'Logística');
